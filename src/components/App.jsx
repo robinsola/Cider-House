775eef49accb1cee76d1512872a81196ed2668aa
Robin Sola <rsolarms@gmail.com>
2018-06-29 08:34:28 -0700

    create react template files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Router from './Router';
+
+function App() {
+  return (
+    <div>
+      <h1>App Works</h1>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Router.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function Router() {
+  return (
+    <div>
+      <Switch>
+      </Switch>
+    </div>
+  );
+}
+
+export default Router;
