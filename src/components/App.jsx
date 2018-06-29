7eb06152a86579ea91ad6db15d0a9469bbfef56d
Robin Sola <rsolarms@gmail.com>
2018-06-29 09:51:38 -0700

    add Home component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,12 @@ import Navbar from './Navbar';
 function App() {
   return (
     <div>
+      <style jsx global>{`
+        * {
+          margin: 0;
+          padding: 0;
+        }
+      `}</style>
       <Navbar />
       <Router />
     </div>
