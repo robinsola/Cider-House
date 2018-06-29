b90560559153bf947cf64ad11a674bd9ebec4516
Robin Sola <rsolarms@gmail.com>
2018-06-29 13:52:25 -0700

    import logos into CiderList

--- src/components/Cider.jsx
+++ src/components/Cider.jsx
@@ -4,11 +4,23 @@ import PropTypes from 'prop-types';
 function Cider(props) {
   return (
     <div>
-      <h4>{props.name}, {props.brewer}</h4>
-      <p>{props.description}</p>
-      <p>abv: {props.abv}</p>
-      <p>${props.price} per pint</p>
-      <p>pints left: {props.remaining}</p>
+      <style jsx>{`
+        img {
+          width: 100px;
+        }
+        .ciderGroup {
+          width: 300px;
+          margin: 0 0 40px 0;
+        }
+      `}</style>
+      <div className="ciderGroup">
+        <img src={props.image}/>
+        <h4>{props.name}, {props.brewer}</h4>
+        <p>{props.description}</p>
+        <p>abv: {props.abv}</p>
+        <p>${props.price} per pint</p>
+        <p>pints left: {props.remaining}</p>
+      </div>
     </div>
   );
 }
@@ -19,7 +31,8 @@ Cider.propTypes = {
   description: PropTypes.string,
   abv: PropTypes.string,
   price: PropTypes.string,
-  remaining: PropTypes.string
+  remaining: PropTypes.string,
+  image: PropTypes.string
 };
 
 export default Cider;
--- src/components/CiderList.jsx
+++ src/components/CiderList.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import Cider from './Cider';
+import angryOrchard from '../assets/images/angry-orchard-logo.png';
+import austinEastciders from '../assets/images/austin-eastciders-logo.png';
+import jacksCider from '../assets/images/jacks-cider-logo.png';
+import lonetree from '../assets/images/lonetree-logo.png';
+import pdxCider from '../assets/images/pdx-cider-logo.png';
+import woodchuck from '../assets/images/woodchuck-logo.png';
+import squareMile from '../assets/images/squaremile-logo.png';
 
 const masterCiderList = [
   {
@@ -8,15 +15,17 @@ const masterCiderList = [
     description: 'Oregon grown apples in a truly authentic English Cider',
     abv: '6.9%',
     price: '7',
-    remaining: '20'
+    remaining: '20',
+    image: pdxCider
   },
   {
     name: 'Fever Bark Tonic',
-    brewer: 'Portland Cider Co',
+    brewer: 'Square Mile Cider Co',
     description: 'Juniper berries and Cinchona bark blended in classic dry NW apple cider',
     abv: '6.3%',
     price: '6',
-    remaining: '60'
+    remaining: '60',
+    image: squareMile
   },
   {
     name: 'Ruby Red Grapefruit',
@@ -24,7 +33,8 @@ const masterCiderList = [
     description: 'Zippy, zesty, and refreshingly dry, from Austin Texas',
     abv: '4.7%',
     price: '3',
-    remaining: '65'
+    remaining: '65',
+    image: austinEastciders
   },
   {
     name: 'Peach',
@@ -32,7 +42,8 @@ const masterCiderList = [
     description: 'Light and fruity with a flirty peach aroma',
     abv:  '5.9%',
     price: '6',
-    remaining: '75'
+    remaining: '75',
+    image: jacksCider
   },
   {
     name: 'Rose\' Hard Cider',
@@ -40,7 +51,8 @@ const masterCiderList = [
     description: 'Rare French apples and a kiss of hibiscus, similar to a semi-dry wine',
     abv:  '7.5%',
     price: '6',
-    remaining: '18'
+    remaining: '18',
+    image: angryOrchard
   },
   {
     name: 'Apple Ginger Cider',
@@ -48,7 +60,8 @@ const masterCiderList = [
     description: 'British-Columbia craft cider with a zesty twist of real ginger',
     abv:  '5.5%',
     price: '6',
-    remaining: '58'
+    remaining: '58',
+    image: lonetree
   },
   {
     name: 'Amber',
@@ -56,23 +69,39 @@ const masterCiderList = [
     description: 'blend of apples and fermented with Champagne, the original blend from Middlebury, Vermont',
     abv: '6.9%',
     price: '7',
-    remaining: '20'
+    remaining: '20',
+    image: woodchuck
   }
 ];
 
 function CiderList() {
   return (
     <div>
-      <h1>Cider List</h1>
+      <style jsx>{`
+        .title {
+          text-align: center;
+          margin: 20px 0 40px 0;
+        }
+        .body {
+          margin: 0 0 0 40px;
+        }
+      `}</style>
+      <div className="title">
+        <h1>Cider List</h1>
+        <p>7 Rotating Taps of Craft Ciders</p>
+      </div>
+      <div className="body">
         {masterCiderList.map((cider, index) =>
-        <Cider name={cider.name}
-          brewer={cider.brewer}
-          description={cider.description}
-          abv={cider.abv}
-          price={cider.price}
-          remaining={cider.remaining}
-          key={index} />
+          <Cider name={cider.name}
+            brewer={cider.brewer}
+            description={cider.description}
+            abv={cider.abv}
+            price={cider.price}
+            remaining={cider.remaining}
+            image={cider.image}
+            key={index} />
         )}
+      </div>
     </div>
   );
 }
