ec1b5c2bc2915113476f0cb066fb75d045cc3396
Robin Sola <rsolarms@gmail.com>
2018-06-29 14:46:46 -0700

    add About component to router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
 import CiderList from './CiderList';
 
 
@@ -9,6 +10,7 @@ function Router() {
     <div>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
         <Route path='/CiderList' component={CiderList} />
       </Switch>
     </div>
