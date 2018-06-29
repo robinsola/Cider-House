3fa68291c9d34df2ae5f9a439492c35d7c727d66
Robin Sola <rsolarms@gmail.com>
2018-06-29 15:37:10 -0700

    add footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <style jsx>{`
+
+      `}</style>
+      <ul>
+        <li>The Cider House</li>
+        <li>400 SW 6th Avenue | Portland, Oregon</li>
+        <li>518-585-5250</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
