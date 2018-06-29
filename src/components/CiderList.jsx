9ccdd25ff11ed7f3b31c5a7d2356e98844dc6391
Robin Sola <rsolarms@gmail.com>
2018-06-29 11:22:57 -0700

    add masterCiderList object

---
+++ src/components/CiderList.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+
+
+const masterCiderList = [
+  {
+    name: 'Ruby Zozzle',
+    brewer: 'Hi-Wheel',
+    description: 'Sparkling Wine & Grapefruit',
+    abv: '6.8%',
+    price: '7',
+    remaining: '20'
+  },
+  {
+    name: 'Tart N Juicy',
+    brewer: 'Epic',
+    description: 'Sour IPA',
+    abv: '4.5%',
+    price: '6',
+    remaining: '60'
+  },
+  {
+    name: 'Hamm\'s',
+    brewer: 'Miller/Coors',
+    description: 'American Lager',
+    abv: '4.7%',
+    price: '3',
+    remaining: '65'
+  },
+  {
+    name: 'Prismatic',
+    brewer: 'Ninkasi',
+    description: 'Juicy IPA',
+    abv:  '5.9%',
+    price: '6',
+    remaining: '75'
+  },
+  {
+    name: 'Juicy Haze',
+    brewer: 'New Belgium',
+    description: 'India Pale Ale',
+    abv:  '7.5%',
+    price: '6',
+    remaining: '18'
+  },
+  {
+    name: '8 Hop',
+    brewer: 'New Belgium',
+    description: 'Pale Ale',
+    abv:  '5.5%',
+    price: '6',
+    remaining: '58'
+  }
+];
+
+function CiderList() {
+  return (
+    <div>
+      {masterCiderList.map((cider, index) =>
+      <Cider name={cider.name}
+        brewer={cider.brewer}
+        description={cider.description}
+        abv={cider.abv}
+        price={cider.price}
+        remaining={cider.remaining}
+        key={index} />
+      )}
+    </div>
+  );
+}
+
+export default CiderList;
