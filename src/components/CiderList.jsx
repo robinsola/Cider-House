84d31542a6dcbbc3ee14ac032076e553f1043f22
Robin Sola <rsolarms@gmail.com>
2018-06-29 12:09:14 -0700

    update cider list with ciders

--- src/components/CiderList.jsx
+++ src/components/CiderList.jsx
@@ -3,52 +3,60 @@ import Cider from './Cider';
 
 const masterCiderList = [
   {
-    name: 'Ruby Zozzle',
-    brewer: 'Hi-Wheel',
-    description: 'Sparkling Wine & Grapefruit',
-    abv: '6.8%',
+    name: 'Union Jack',
+    brewer: 'Portland Cider Co',
+    description: 'Oregon grown apples in a truly authentic English Cider',
+    abv: '6.9%',
     price: '7',
     remaining: '20'
   },
   {
-    name: 'Tart N Juicy',
-    brewer: 'Epic',
-    description: 'Sour IPA',
-    abv: '4.5%',
+    name: 'Fever Bark Tonic',
+    brewer: 'Portland Cider Co',
+    description: 'Juniper berries and Cinchona bark blended in classic dry NW apple cider',
+    abv: '6.3%',
     price: '6',
     remaining: '60'
   },
   {
-    name: 'Hamm\'s',
-    brewer: 'Miller/Coors',
-    description: 'American Lager',
+    name: 'Ruby Red Grapefruit',
+    brewer: 'Austin Eastciders',
+    description: 'Zippy, zesty, and refreshingly dry, from Austin Texas',
     abv: '4.7%',
     price: '3',
     remaining: '65'
   },
   {
-    name: 'Prismatic',
-    brewer: 'Ninkasi',
-    description: 'Juicy IPA',
+    name: 'Peach',
+    brewer: 'Jack\'s Hard Cider',
+    description: 'Light and fruity with a flirty peach aroma',
     abv:  '5.9%',
     price: '6',
     remaining: '75'
   },
   {
-    name: 'Juicy Haze',
-    brewer: 'New Belgium',
-    description: 'India Pale Ale',
+    name: 'Rose\' Hard Cider',
+    brewer: 'Angry Orchard',
+    description: 'Rare French apples and a kiss of hibiscus, similar to a semi-dry wine',
     abv:  '7.5%',
     price: '6',
     remaining: '18'
   },
   {
-    name: '8 Hop',
-    brewer: 'New Belgium',
-    description: 'Pale Ale',
+    name: 'Apple Ginger Cider',
+    brewer: 'Lonetree',
+    description: 'British-Columbia craft cider with a zesty twist of real ginger',
     abv:  '5.5%',
     price: '6',
     remaining: '58'
+  },
+  {
+    name: 'Amber',
+    brewer: 'Woodchuck Hard Cider',
+    description: 'blend of apples and fermented with Champagne, the original blend from Middlebury, Vermont',
+    abv: '6.9%',
+    price: '7',
+    remaining: '20'
   }
 ];
 
