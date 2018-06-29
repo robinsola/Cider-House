4c6f09d79628fd44449c79153ea0c4e6b79fb358
Robin Sola <rsolarms@gmail.com>
2018-06-29 14:47:17 -0700

    add image background

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import interior from '../assets/images/interior.jpg';
+
+function About() {
+  return (
+    <div>
+      <style jsx>{`
+        .header {
+          background: url(${interior});
+          background-position: center;
+          height: 500px;
+          color: white;
+          text-align: center;
+        }
+      `}</style>
+      <div className="header">
+      </div>
+    </div>
+  );
+}
+
+export default About;
