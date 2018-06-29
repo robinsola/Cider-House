f03b488498b5361f42237fdd8d5b599072583471
Robin Sola <rsolarms@gmail.com>
2018-06-29 15:48:58 -0700

    add media query to make footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,21 @@ function Footer() {
   return (
     <div>
       <style jsx>{`
-
+        ul {
+          display: flex;
+          justify-content: space-around;
+          list-style: none;
+          padding: 50px;
+          background-color: #221800;
+          color: white;
+        }
+        @media only screen and (max-width: 700px) {
+          ul {
+            display: block;
+            text-align: center;
+            line-height: 30px;
+          }
+        }
       `}</style>
       <ul>
         <li>The Cider House</li>
