431e5a7b45f96dec86bcd9a84f3fb49d55abecb5
Robin Sola <rsolarms@gmail.com>
2018-07-06 14:45:19 -0700

    change App back to function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,25 @@ import Router from './Router';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-class App extends React.Component {
+function App() {
 
-  render() {
-    return (
-      <div>
-        <style jsx global>{`
-          * {
-            margin: 0;
-            padding: 0;
-          }
-          body {
-            font-family: sans-serif;
-            font-weight: 100;
-          }
-        `}</style>
-        <Navbar />
-        <Router />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <style jsx global>{`
+        * {
+          margin: 0;
+          padding: 0;
+        }
+        body {
+          font-family: sans-serif;
+          font-weight: 100;
+        }
+      `}</style>
+      <Navbar />
+      <Router />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
